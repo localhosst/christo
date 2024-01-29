9fe3c81909b467124f286caac0940a65780e9890
localhosst <ukusa721@gmail.com>
2024-01-29 23:25:30 +0530

    deleted

--- homeframes.php
+++
@@ -1,9 +0,0 @@
-<html>
-    <head>
-        <title> Home </title>
-    </head>
-<frameset cols="20%,80%">
-    <frame src="menu.html" name="frame1">
-    <frame src="home1.html" name="frame2">   
-</frameset>
-</html>
